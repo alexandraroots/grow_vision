0fc893d69be8ed7dd4f235032a05af0fe1e9bf09
Sergey Berezin <sergey@itis.cs.msu.ru>
2021-01-22 14:48:05 +0300

    Serilog-based logging is added with Console, File and ApplicationInsights sinks.

---
+++ grow/Logger.cs
@@ -0,0 +1,52 @@
+using Serilog.Core;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+using System;
+using Serilog;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+
+namespace growtimelapse 
+{
+    public static class LoggerFactory 
+    {
+        public const string LoggerOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Properties} {Message:lj}{NewLine}{Exception}";
+
+        public const string AppInsightsKeySetting = "ApplicationInsights:InstrumentationKey";
+
+        public const string LogFileName = "log.txt";
+
+        public static Logger CreateLogger() 
+        {
+            var loggerConfig = new LoggerConfiguration()
+                .WriteTo.File(
+                    outputTemplate: LoggerOutputTemplate,
+                    path: Path.Combine(Configuration.RootFolder, LogFileName),
+                    rollingInterval: RollingInterval.Day)
+                .ReadFrom.Configuration(Configuration.Instance);
+
+            var appInsightsKey = Configuration.Instance.GetValue<string>(AppInsightsKeySetting, null);
+            if (!string.IsNullOrEmpty(appInsightsKey))
+            {
+                // Note that AppInsights sink ignores the output template if given in the json configuration,
+                // and we can't provide it in code as well.
+                // See https://stackoverflow.com/questions/57183780/using-outputtemplate-argument-in-serilog-with-azure-application-insights
+                loggerConfig = loggerConfig.WriteTo.ApplicationInsights(new TelemetryConfiguration()
+                {
+                    InstrumentationKey = appInsightsKey,
+                    TelemetryChannel = new ServerTelemetryChannel(),
+                }, TelemetryConverter.Traces);
+            }
+
+            var logger = loggerConfig.CreateLogger();
+
+            if(string.IsNullOrEmpty(appInsightsKey)) 
+            {
+                logger.Warning($"{AppInsightsKeySetting} setting is not found or empty. ApplicationInsights logging is not enabled.");
+            }
+
+            return logger;
+        }
+    }
+}
--- grow/Program.cs
+++ grow/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
+using Serilog;
 
 namespace growtimelapse
 {
@@ -11,30 +12,50 @@ namespace growtimelapse
 
         private readonly static string cameraIP = Environment.GetEnvironmentVariable("CAMERA_IP");
         private static string cameraUrl;
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            cameraUrl = "http://" + cameraIP + "//ISAPI/Streaming/Channels/101/picture?snapShotImageType=JPEG";
-            ImageClient currentClient = new ImageClient();
-            Store store = new Store("timelapse1");
-            TimeSpan interval = new TimeSpan(1, 0, 0);
-            while (true)
+            ILogger logger = null;
+            try 
             {
-                try
-                {
-                    HttpResponseMessage response = await currentClient.client.GetAsync(cameraUrl);
-                    response.EnsureSuccessStatusCode();
-                    Stream responseImage = await response.Content.ReadAsStreamAsync();
-
-                    BlobClient blobClient = store.containerClient.GetBlobClient(DateTime.Now.ToString("yyyyMMddHHmmss") + ".JPEG");
-                    await blobClient.UploadAsync(responseImage, true);
-
-                }
-                catch (Exception) {
-                }
-
-                System.Threading.Thread.Sleep(interval);
-
+                logger = LoggerFactory.CreateLogger();
             }
+            catch(Exception exc) 
+            {
+                Console.WriteLine($"Error creating logger: {exc}");
+                return 1;
+            }
+
+            logger.Information($"Timelapse service is starting. Camera host is {cameraIP}.");
+
+                        Console.ReadLine();
+return 0;
+
+        //     cameraUrl = "http://" + cameraIP + "//ISAPI/Streaming/Channels/101/picture?snapShotImageType=JPEG";
+        //     ImageClient currentClient = new ImageClient();
+        //     Store store = new Store("timelapse1");
+        //     TimeSpan interval = new TimeSpan(1, 0, 0);
+        //     while (true)
+        //     {
+        //         try
+        //         {
+        //             logger.Information("Capturing a frame...");
+
+        //             HttpResponseMessage response = await currentClient.client.GetAsync(cameraUrl);
+        //             response.EnsureSuccessStatusCode();
+        //             Stream responseImage = await response.Content.ReadAsStreamAsync();
+
+        //             logger.Information("Sending the frame to cloud...");
+
+        //             BlobClient blobClient = store.containerClient.GetBlobClient(DateTime.Now.ToString("yyyyMMddHHmmss") + ".JPEG");
+        //             await blobClient.UploadAsync(responseImage, true);
+
+        //         }
+        //         catch (Exception exc) 
+        //         {
+        //             logger.Error(exc, "Error capturing a frame");
+        //         }
+
+        //         System.Threading.Thread.Sleep(interval);
 
         }
 
